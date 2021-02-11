472684ea6c49ac38c7cbe84bbe82accab52b36e2
ShadowZ11 <72619789+ShadowZ11@users.noreply.github.com>
2021-02-12 00:33:34 +0100

    Update Projet_OS.layout

--- main.c
+++ main.c
@@ -10,21 +10,21 @@
 
 int main()
 {
-      char    buffer[TAILLE_BUFFER];
+      char buffer[TAILLE_BUFFER];
 
 
     while(1)
     {
-        printf("Prompt : ");
+        printf("choisissez votre commande: ");
         fgets(buffer,TAILLE_BUFFER,stdin);
         buffer[strlen(buffer)-1]='\0';
         if (strcmp("exit",buffer)==0)
         {
-            exit(0);
+            exit(0); //cette partie permet de mettre en place la commande exit qui va arrêter le processus du programme
         }
         char *arg_list[32];
         char *p=strdup(buffer);
-        char *tmp=strtok(p," ");
+        char *tmp=strtok(p," "); //création du tableau permettant la création des bouts de ligne entre caractères espaces
         int increment=0;
         while (tmp!=NULL)
         {
@@ -52,21 +52,21 @@ int main()
             tmp=strtok(NULL," ");
         }
         arg_list[increment]=NULL;
-        pid_t process=fork();
-        if (process==0)
+        pid_t process=fork(); //création du processus fils avec la commande fork
+        if (process==0) //si le processus père est égal à 0
         {
             int retour=execvp(arg_list[0],arg_list);
-            if (retour==-1) printf("%s\n",strerror(errno));
+            if (retour==-1) printf("%s\n",strerror(errno)); //on se met dans le processus fils qui remplace le processus actuel par le contenu des paramètres passés et exécute celui-ci
             exit(0);
         }
         else
         {
-            wait(&process);
+            wait(&process); // si process = 0 on se met en attente de la fin du processus fils
         }
         increment=0;
         while (arg_list[increment]!=NULL)
         {
-            free(arg_list[increment]);
+            free(arg_list[increment]); // à la fin du process on libère la mémoire du la liste d'argument
             increment++;
         }
         free(p);
